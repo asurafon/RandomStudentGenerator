98f00fd11db29c9fc29e523b4ed068e5fefc29e6
asuraTSE <asura@tse.go.cr>
2019-02-04 13:40:31 -0600

    adding files Random Student

---
+++ src/components/RandomStudent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Random Studentasdf
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'randomStudent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
